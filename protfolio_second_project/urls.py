bb7166222be21483a923685fdffd8a18ae7b1796
Umair <26513umair@gmail.com>
2019-09-24 00:41:01 +0530

    Home page created

--- protfolio_second_project/urls.py
+++ protfolio_second_project/urls.py
@@ -19,8 +19,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
+from Python_Django.protfolio_second_project.jobs import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.home, name='home'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
